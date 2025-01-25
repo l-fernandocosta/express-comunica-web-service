7dc965afe6a21d544b75f309c5d159e3c005b54f
fer.tsx <fernandocostadev98@gmail.com>
2025-01-25 20:58:17 -0300

    feat: add create-user-validation

---
+++ src/infra/validation/zod/user/create-user.validation.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+const createUserValidator = z.object({
+  name: z.string().min(1, 'Name is required'),
+  email: z.string().email('Invalid email format'),
+  password: z.string().min(8),
+});
+
+export { createUserValidator as createUserSchema };
