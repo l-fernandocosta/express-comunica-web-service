2a2ed175f77fdb1fcacb5d21eac6383657da43d1
fer.tsx <fernandocostadev98@gmail.com>
2025-01-25 20:51:32 -0300

    chore(config): add jest-config

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,15 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
-  coverageProvider: "v8",
-  testEnvironment: "node", 
-  moduleFileExtensions: ['ts', 'js'], 
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
   transform: {
-     '^.+\\.(t|j)sx?$': '@swc/jest',
-  }, 
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-  }
+  },
 };
 
 export default config;
