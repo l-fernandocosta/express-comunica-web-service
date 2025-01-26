cfb092fe068a242966c65826a29d32b86f1bf8c1
fer.tsx <fernandocostadev98@gmail.com>
2025-01-25 21:00:41 -0300

    feat: add auth and user controllers

---
+++ src/infra/http/controllers/auth.controller.ts
@@ -0,0 +1,36 @@
+import Password from '@/domain/value-objects/password.vo';
+import { prisma } from '@/infra/persistence/prisma/prisma-client';
+import JwtService from '@/infra/security/jwt/jwt.service';
+import express from 'express';
+import {
+  BaseHttpController,
+  controller,
+  httpPost,
+  request,
+} from 'inversify-express-utils';
+
+@controller('/auth')
+export default class AuthController extends BaseHttpController {
+  @httpPost('/login')
+  public async login(@request() request: express.Request) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: request.body.email,
+      },
+    });
+
+    if (!user) {
+      return this.json({ error: 'USER_NOT_FOUND' }, 404);
+    }
+
+    const isPasswordOK = Password.verify(request.body.password, user.password);
+
+    if (!isPasswordOK) {
+      return this.json({ error: 'INVALID_PASSWORD' }, 401);
+    }
+
+    const token = JwtService.generate({ userid: user.id });
+
+    return this.json({ token, userId: user.id });
+  }
+}
---
+++ src/infra/http/controllers/user.controller.ts
@@ -0,0 +1,54 @@
+import IUserService from '@/application/services/user-service.interface';
+import { TYPES } from '@/infra/config/di/types';
+import { Request } from 'express';
+import { inject } from 'inversify';
+import {
+  BaseHttpController,
+  controller,
+  httpDelete,
+  httpGet,
+  httpPost,
+  httpPut,
+  request,
+} from 'inversify-express-utils';
+import { authenticate } from '../middleware/private-route.middleware';
+
+@controller('/users')
+export default class UserController extends BaseHttpController {
+  @inject(TYPES.UserService)
+  private readonly userService!: IUserService;
+
+  @httpPost('/')
+  async create(@request() req: Request) {
+    const user = await this.userService.createUser(req.body);
+    return this.created(`/users`, user);
+  }
+
+  @httpGet('/', authenticate)
+  async list() {
+    const users = await this.userService.listUsers();
+    return this.ok(users);
+  }
+
+  @httpGet('/:id', authenticate)
+  async findById(@request() req: Request) {
+    const user = await this.userService.getUser(req.params.id);
+    return this.ok(user);
+  }
+
+  @httpPut('/:id', authenticate)
+  async update(@request() req: Request) {
+    await this.userService.updateUser({
+      id: req.params.id,
+      ...req.body,
+    });
+
+    return this.ok();
+  }
+
+  @httpDelete('/:id', authenticate)
+  async delete(@request() req: Request) {
+    await this.userService.deleteUser(req.params.id);
+    return this.ok();
+  }
+}
